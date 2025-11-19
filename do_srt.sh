a20dfc07a30ee966f03fb413aceabece45c31cc3
Bjørn Kristiansen <post@bhkristiansen.com>
2025-11-19 01:04:42 +0100

    update

--- do_srt.sh
+++ do_srt.sh
@@ -18,13 +18,15 @@ SECONDS=0
 DATO_START=$(date '+%H:%M')
 for FIL in *.en.srt; do
     python translate.py "$FIL" --skip-consistency
+    python3 translate.py "$FIL" --skip-consistency -b 5 -m gemma3:12b
     wait
     let "COUNT++"
-    echo "${GREEN}------------------------------------------------${END}"
+    echo "${GREEN}Finished file ${COUNT}------------------------------------------------${END}"
 done
 DATO_END=$(date '+%H:%M')
 duration=$SECONDS
 echo "${GREEN}**************${END}"
-echo "Translated ${COUNT} files in ${YELLOW}$((duration / 60)) minutes${END} and ${GREEN}$((duration % 60)) seconds${END}"
+echo "Translated ${COUNT} files in ${YELLOW}$(($diff / 3600)) hours${END}, ${GREEN}$((($diff / 60) % 60)) minutes${END} and ${CYAN}$(($diff % 60)) seconds${END}"
+#echo "Translated ${COUNT} files in ${YELLOW}$((duration / 60)) minutes${END} and ${GREEN}$((duration % 60)) seconds${END}"
 echo "Start: ${DATO_START} End: ${DATO_END}"
 echo "${GREEN}**************${END}"
